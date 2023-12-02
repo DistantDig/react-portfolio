30d3841f0e81f8c267f4e0d44bcf4965a72ccdf7
DistantDig <jameswaller711@gmail.com>
2023-12-02 15:54:47 -0800

    wrote main.jsx

---
+++ main.jsx
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App/>,
+        errorElement: <Error/>,
+        children: [
+        ]
+    }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router}/>
+)
