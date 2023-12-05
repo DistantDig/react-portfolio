d2d9a83930ff66598b1ed45d223d052b870a7d56
DistantDig <jameswaller711@gmail.com>
2023-12-04 20:08:49 -0800

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
     return (
         <>
             <Header/>
-            <main className=''>
+            <main className='d-flex flex-column'>
                 <Outlet/>
             </main>
+            <Footer/>
         </>
     );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+
+    return (
+        <footer className="fixed-bottom">
+            <ul className="list-group list-group-horizontal justify-content-center">
+                <li className="list-group-item">(555)-555-5555</li>
+                <li className="list-group-item">jameswaller711@gmail.com</li>
+                <li className="list-group-item"><a className="violet-link" target="#" href="https://github.com/DistantDig">Github</a></li>
+            </ul>
+        </footer>
+    );
+}
+
+export default Footer;
