2227c09ec82cf20618a6836f0a5f86e63a5af504
DistantDig <jameswaller711@gmail.com>
2023-12-18 12:15:48 -0800

    tweaked about me page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,7 +10,7 @@ export default function About() {
             <div className="col-md-10">
               <p>
                 Hiya! My name is James Waller Jr. but I also go by DistantDig online. My main passion is creating through code, and I work each day to grow in my skills. 
-                I currently am learning how to write in HTML, CSS and JavaScript for the purpose of full stack web design. I also enjoy coding in my free time but that 
+                I recently completed a bootcamp for learning how to write in HTML, CSS and JavaScript for the purpose of full stack web design. I also enjoy coding in my free time but that 
                 looks more like modding games through Java. You can check out some of the things I've done so far in the Projects tab!
               </p>
             </div>
