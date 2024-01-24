f2273ac9cdb3b115e8c86527cd073042ccd8261f
DistantDig <jameswaller711@gmail.com>
2024-01-23 17:41:08 -0800

    styled project page better

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,42 +8,47 @@ export default function Projects() {
             description: "Minecraft mod coded in Java. I made this one working with a friend who goes by Kekie6 online. Our newest project.",
             imageName: "immersive_jungels_logo.png",
             imageAlt: "Immersive Jungles Logo",
-            projectLink: "https://www.curseforge.com/minecraft/mc-mods/immersive-jungles"
+            demo: "https://www.curseforge.com/minecraft/mc-mods/immersive-jungles",
+            github: "#"
         },
         readZen: {
             title: "ReadZen",
             description: "Website application for generating and sharing READMEs for project developers.",
             imageName: "readzen_logo.png",
             imageAlt: "ReadZen Logo",
-            projectLink: "https://github.com/Migsrkrd/ReadZen"
+            demo: "https://read-zen.onrender.com",
+            github: "https://github.com/Migsrkrd/ReadZen"
         },
         gymFlow: {
             title: "Gym Flow",
             description: "Website application of a workout-themed social media app for creating and sharing workout routines.",
             imageName: "gym_flow_logo.png",
             imageAlt: "Gym Flow Logo",
-            projectLink: "https://github.com/d-a-v-i-d-w-r-i-g-h-t/gym-flow"
+            demo: "https://sheltered-inlet-69891-1c93b8a30eb8.herokuapp.com/",
+            github: "https://github.com/d-a-v-i-d-w-r-i-g-h-t/gym-flow"
         },
         dndOrigins: {
             title: "Dig's DnD Origins Mod",
             description: "Minecraft mod coded in Java. I made this one on my own and is my most popular project currently.",
             imageName: "digs_dnd_origins_logo.png",
             imageAlt: "DnD Origins Logo",
-            projectLink: "https://www.curseforge.com/minecraft/mc-mods/digs-dnd-origins"
+            demo: "https://www.curseforge.com/minecraft/mc-mods/digs-dnd-origins",
+            github: "https://github.com/DistantDig/digs-dnd-origins"
         },
         treeLib: {
             title: "TreeLib Mod",
             description: "Minecraft mod coded in Java. This is another project coded with Kekie6, and is designed to be a library for future mod projects.",
             imageName: "tree_lib_logo.png",
             imageAlt: "TreeLib Logo",
-            projectLink: "https://www.curseforge.com/minecraft/mc-mods/tree-lib"
+            demo: "https://www.curseforge.com/minecraft/mc-mods/tree-lib",
+            github: "#"
         }
         // contentAId: {
         //     title: "Content AId",
         //     description: "Website application designed to search and evaluate articles on the likelihood of being written by AI.",
         //     imageName: "AId_logo.png",
         //     imageAlt: "AId Logo",
-        //     projectLink: "https://github.com/stellyes/content-AId"
+        //     github: "https://github.com/stellyes/content-AId"
         // }
     }
 
